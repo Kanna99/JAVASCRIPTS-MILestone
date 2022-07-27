da4b4bf003fffdd340e4fd935fe87918a9854c18
aparnachoudary <109720369+Kanna99@users.noreply.github.com>
2022-07-27 17:21:32 +0530

    Add files via upload

---
+++ JAVASCRIPT FILE =2.js
@@ -0,0 +1,16 @@
+funtion reverseString(str){
+    return str;
+
+}
+reverseString("Talentio");
+
+ // step1. use the split()method to return a new array
+ var splitString = str.split(""); // var splitstring = "Talentio".split("");
+ //["T","a","l","e","n","t","i","o"]
+
+
+ return the reversed String
+ return joinArray;//"oitnelaT"
+
+
+ reversestring("Talentio");
